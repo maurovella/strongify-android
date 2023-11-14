97e170ae8d8d35e8bd35e52e6e7845e73e6c62a1
smedin <smedin@itba.edu.ar>
2023-11-14 17:18:22 -0300

    added secuential routine screen

---
+++ app/src/main/java/com/example/strongify/ui/screens/SecuentialRoutineScreen.kt
@@ -0,0 +1,147 @@
+package com.example.strongify.ui.screens
+
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.strongify.R
+
+@Preview
+@Composable
+fun SecuentialRoutineScreen() {
+
+    var dropdown by remember {
+        mutableStateOf(false)
+    }
+    val context = LocalContext.current.applicationContext
+    
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Icon(
+                imageVector = Icons.Filled.ArrowBack,
+                contentDescription = "",
+                tint = Color.White,
+            )
+        }
+        Text(text = "hhhhhh", color = Color.Red)
+        Spacer(modifier = Modifier.height(20.dp))
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(Color.Red)
+                    .height(60.dp)
+                    .width(100.dp)
+                    .clickable { dropdown = true }
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Text(text = "secuencial", color = Color.White)
+                        IconButton(
+                            onClick = {
+                                dropdown = true
+                            }
+                        ){
+                            Icon(
+                                imageVector = Icons.Filled.KeyboardArrowDown,
+                                contentDescription = "",
+                                tint = Color.White
+                            )
+                        }
+                        DropdownMenu(
+                            expanded = dropdown,
+                            onDismissRequest = { dropdown = false })
+                        {
+                            DropdownMenuItem(
+                                text = { Text(text = "hola") },
+                                onClick = { Toast.makeText(context, "messi", Toast.LENGTH_LONG).show() }
+                            )
+                        }
+                    }
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Image(
+            painter = painterResource(R.drawable.ejercicio_1),
+            contentDescription = null,
+            modifier = Modifier.size(350.dp)
+        )
+        Text(text = "mucho texto", color = Color.White)
+        Text(text = "mucho texto", color = Color.White)
+        Text(text = "mucho texto", color = Color.White)
+        Text(text = "mucho texto", color = Color.White)
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "-")
+            }
+            Text(text = "mucho texto", color = Color.White)
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "+")
+            }
+        }
+        Button(
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
+            onClick = { /*TODO*/ }
+        ) {
+            Text(text = "Proximo ejercicio")
+        }
+        Button(
+            //colors = ButtonDefaults.buttonColors(containerColor = Color.Gray),
+            onClick = { /*TODO*/ }
+        ) {
+            Text(text = "Terminar")
+        }
+    }
+    
+}
